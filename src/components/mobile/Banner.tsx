d914044c9d7f8e7e9445e93f080cf49f14d4f78d
mikamokedo <mikamokedo@gmail.com>
2025-10-21 08:50:26 +0200

    fix: style

--- src/components/mobile/Banner.tsx
+++ src/components/mobile/Banner.tsx
@@ -1,36 +1,42 @@
-import React from "react";
-
 export const Banner = () => {
-    return (
-        <div className="grid-cols-2 grid gap-[12px]">
-            <div className="relative overflow-hidden bg-[#FFAD3B] py-[4px] px-[8px] rounded-t-[4px] h-[75px]">
-                <div className="text-[12px] font-[700] text-[#333333]">Tên nhóm danh mục <br /> thiết yếu</div>
-                <div className="flex absolute top-[10px] right-[0px]">
-                    <img src="/src/assets/banner.svg" className="z-10 w-[78px] h-[73px]" alt="" />
-                </div>
-                <div className="w-[154px] h-[154px] bg-[#fff] opacity-50 absolute top-[5px] right-[-75px] rounded-full"></div>
-            </div>
-            <div className="relative overflow-hidden bg-[#8EEE6B] py-[4px] px-[8px] rounded-t-[4px] h-[75px]">
-                <div className="text-[12px] font-[700] text-[#333333]">Tên nhóm danh mục <br /> thiết yếu</div>
-                <div className="flex absolute top-[10px] right-[0px]">
-                    <img src="/src/assets/banner.svg" className="z-10 w-[78px] h-[73px]" alt="" />
-                </div>
-                <div className="w-[154px] h-[154px] bg-[#fff] opacity-50 absolute top-[5px] right-[-75px] rounded-full"></div>
-            </div>
-            <div className="relative overflow-hidden bg-[#BABDFB] py-[4px] px-[8px] rounded-t-[4px] h-[75px]">
-                <div className="text-[12px] font-[700] text-[#333333]">Tên nhóm danh mục <br /> thiết yếu</div>
-                <div className="flex absolute top-[10px] right-[0px]">
-                    <img src="/src/assets/banner.svg" className="z-10 w-[78px] h-[73px]" alt="" />
-                </div>
-                <div className="w-[154px] h-[154px] bg-[#fff] opacity-50 absolute top-[5px] right-[-75px] rounded-full"></div>
-            </div>
-            <div className="relative overflow-hidden bg-[#FFA2D5] py-[4px] px-[8px] rounded-t-[4px] h-[75px]">
-                <div className="text-[12px] font-[700] text-[#333333]">Tên nhóm danh mục <br /> thiết yếu</div>
-                <div className="flex absolute top-[10px] right-[0px]">
-                    <img src="/src/assets/banner.svg" className="z-10 w-[78px] h-[73px]" alt="" />
-                </div>
-                <div className="w-[154px] h-[154px] bg-[#fff] opacity-50 absolute top-[5px] right-[-75px] rounded-full"></div>
-            </div>
+  return (
+    <div className="grid-cols-2 grid gap-[12px]">
+      <div className="relative overflow-hidden bg-[#FFAD3B] py-[4px] px-[8px] rounded-t-[4px] h-[75px]">
+        <div className="text-[12px] font-[700] text-[#333333]">
+          Tên nhóm danh mục <br /> thiết yếu
         </div>
-    )
-}
+        <div className="flex absolute top-[10px] right-[0px]">
+          <img src="/assets/banner.svg" className="z-10 w-[78px] h-[73px]" alt="" />
+        </div>
+        <div className="w-[154px] h-[154px] bg-[#fff] opacity-50 absolute top-[5px] right-[-75px] rounded-full"></div>
+      </div>
+      <div className="relative overflow-hidden bg-[#8EEE6B] py-[4px] px-[8px] rounded-t-[4px] h-[75px]">
+        <div className="text-[12px] font-[700] text-[#333333]">
+          Tên nhóm danh mục <br /> thiết yếu
+        </div>
+        <div className="flex absolute top-[10px] right-[0px]">
+          <img src="/assets/banner.svg" className="z-10 w-[78px] h-[73px]" alt="" />
+        </div>
+        <div className="w-[154px] h-[154px] bg-[#fff] opacity-50 absolute top-[5px] right-[-75px] rounded-full"></div>
+      </div>
+      <div className="relative overflow-hidden bg-[#BABDFB] py-[4px] px-[8px] rounded-t-[4px] h-[75px]">
+        <div className="text-[12px] font-[700] text-[#333333]">
+          Tên nhóm danh mục <br /> thiết yếu
+        </div>
+        <div className="flex absolute top-[10px] right-[0px]">
+          <img src="/assets/banner.svg" className="z-10 w-[78px] h-[73px]" alt="" />
+        </div>
+        <div className="w-[154px] h-[154px] bg-[#fff] opacity-50 absolute top-[5px] right-[-75px] rounded-full"></div>
+      </div>
+      <div className="relative overflow-hidden bg-[#FFA2D5] py-[4px] px-[8px] rounded-t-[4px] h-[75px]">
+        <div className="text-[12px] font-[700] text-[#333333]">
+          Tên nhóm danh mục <br /> thiết yếu
+        </div>
+        <div className="flex absolute top-[10px] right-[0px]">
+          <img src="/assets/banner.svg" className="z-10 w-[78px] h-[73px]" alt="" />
+        </div>
+        <div className="w-[154px] h-[154px] bg-[#fff] opacity-50 absolute top-[5px] right-[-75px] rounded-full"></div>
+      </div>
+    </div>
+  );
+};
--- src/components/mobile/Header.tsx
+++ src/components/mobile/Header.tsx
@@ -1,31 +1,38 @@
 import React from "react";
-import { ChevronDoubleUpIcon } from "@heroicons/react/24/solid";
 
 export const Header: React.FC = () => {
-    return (
-        <div className="z-10 relative bg-[linear-gradient(0deg,#138636,#138636),linear-gradient(0deg,rgba(0,0,0,0.2),rgba(0,0,0,0.2))] p-[12px] flex flex-col gap-[12px]">
-            <div className="z-10 flex justify-between items-center">
-                <div className="flex items-center gap-[12px]">
-                    <img src="/src/assets/arrowLeftMB.svg" alt="" />
-                    <div className="text-[white]">
-                        <span className="text-[12px]">Giao tới</span>
-                        <div className="text-[14px] font-[600]">Địa chỉ giao hàng</div>
-                    </div>
-                </div>
-                <div className="flex gap-[12px]">
-                    <button className="bg-inherit border-none cursor-pointer"><img src="/src/assets/basketHeaderMB.svg" alt="" /></button>
-                    <button className="bg-inherit border-none cursor-pointer"><img src="/src/assets/notificationMB.svg" alt="" /></button>
-                </div>
-            </div>
-            <div className="z-10 w-full relative">
-                <input 
-                    className="pl-[30px] max-w-full w-[calc(100%-40px)] outline-[0px] bg-inherit border-1 border-[white] rounded-[8px] p-[8px] text-[white] placeholder-[white]" 
-                    type="text" 
-                    placeholder="Bạn cần mua gì?" 
-                />
-                <img src="/src/assets/searchMB.svg" className="absolute top-[50%] translate-y-[-50%] left-[10px]" alt="" />
-            </div>
-            <img src="assets/headerMB.jpg" className="transform scale-x-[-1] opacity-25 absolute top-[0px] left-[0px] w-full h-full inset-0 object-cover z-0" alt="" />
+  return (
+    <div className="z-10 relative bg-[linear-gradient(0deg,#138636,#138636),linear-gradient(0deg,rgba(0,0,0,0.2),rgba(0,0,0,0.2))] p-[12px] flex flex-col gap-[12px]">
+      <div className="z-10 flex justify-between items-center">
+        <div className="flex items-center gap-[12px]">
+          <img src="/assets/arrowLeftMB.svg" alt="" />
+          <div className="text-[white]">
+            <span className="text-[12px]">Giao tới</span>
+            <div className="text-[14px] font-[600]">Địa chỉ giao hàng</div>
+          </div>
         </div>
-    )
-}
+        <div className="flex gap-[12px]">
+          <button className="bg-inherit border-none cursor-pointer">
+            <img src="/assets/basketHeaderMB.svg" alt="" />
+          </button>
+          <button className="bg-inherit border-none cursor-pointer">
+            <img src="/assets/notificationMB.svg" alt="" />
+          </button>
+        </div>
+      </div>
+      <div className="z-10 w-full relative">
+        <input
+          className="pl-[30px] max-w-full w-[calc(100%-40px)] outline-[0px] bg-inherit border-1 border-[white] rounded-[8px] p-[8px] text-[white] placeholder-[white]"
+          type="text"
+          placeholder="Bạn cần mua gì?"
+        />
+        <img src="/assets/searchMB.svg" className="absolute top-[50%] translate-y-[-50%] left-[10px]" alt="" />
+      </div>
+      <img
+        src="/assets/headerMB.jpg"
+        className="transform scale-x-[-1] opacity-25 absolute top-[0px] left-[0px] w-full h-full inset-0 object-cover z-0"
+        alt=""
+      />
+    </div>
+  );
+};
